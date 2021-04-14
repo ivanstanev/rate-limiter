1e884c6896af4a437829f8e7468bd8163b6e590f
ivanstanev <istanev93@gmail.com>
2021-04-14 18:55:29 +0100

    chore: fix Redis tests

--- internal/redis/redis_test.go
+++ internal/redis/redis_test.go
@@ -2,13 +2,29 @@ package limiter
 
 import (
 	"testing"
+	"time"
 
+	"github.com/go-redis/redis/v8"
 	limiter "github.com/ivanstanev/rate-limiter/limiter"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestInMemoryRateLimiterShouldLimit(t *testing.T) {
-	rl := RedisRateLimiter{}
+func TestRedisRateLimiterShouldLimit(t *testing.T) {
+	redisClient := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+	rlConfig := &limiter.RateLimiterConfiguration{
+		Window: limiter.Window{
+			Tokens:      0,
+			RefreshRate: time.Minute,
+		},
+	}
+	rl := &RedisRateLimiter{
+		RedisClient:   redisClient,
+		Configuration: rlConfig,
+	}
 	got := rl.ShouldLimit("Boo")
 	want := false
 
